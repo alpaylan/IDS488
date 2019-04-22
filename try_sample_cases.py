730b64cce02cd37927bb1e2575578cd59a6138b0
alpaylan <alpkeles99@gmail.com>
2019-04-22 18:55:54 +0300

    Hyperparameter tuning started

--- try_sample_cases.py
+++ try_sample_cases.py
@@ -15,7 +15,19 @@ features_train, features_test, labels_train, labels_test = train_test_split(feat
 
 from sklearn.ensemble import RandomForestClassifier
 
-rf = RandomForestClassifier(n_estimators = 100 , random_state = 42 , oob_score = True)
+rf = RandomForestClassifier(n_estimators = 200 ,
+                            random_state = 42 , 
+                            oob_score = True ,
+                            class_weight=None, 
+                            criterion='gini', 
+                            max_depth=None,
+                            max_features='auto', 
+                            max_leaf_nodes=None,
+                            min_impurity_decrease=0.0, 
+                            min_impurity_split=None,
+                            min_samples_leaf=1, 
+                            min_samples_split=2,
+                            min_weight_fraction_leaf=0.0)
 
 rf.fit(features_train, labels_train)
 
@@ -23,7 +35,6 @@ rf_predict = rf.predict(features_test)
 
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
-
 rf_cv_score = cross_val_score(rf, features, labels, cv=10, scoring="roc_auc")
 
 print("=== Confusion Matrix ===")
@@ -38,4 +49,9 @@ print('\n')
 print("=== Mean AUC Score ===")
 print("Mean AUC Score - Random Forest: ", rf_cv_score.mean())
 print("oob_score:" , rf.oob_score_)
-print("Feature importances:" ,rf.feature_importances_)
+#print("oob_decision_function:" ,rf.oob_decision_function_)
+#print(rf.estimators_)
+#print("Feature Importances: ", rf.feature_importances_)
+#rf_predict_proba = rf.predict_proba(features_test)
+#print("Predict Class Probabilities", rf_predict_proba)
+print("Score : " , rf.score( features_test,labels_test ,))

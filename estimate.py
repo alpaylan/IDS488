642f28949d7cee719a9d22e040b594297c0a88f3
alpaylan <alpkeles99@gmail.com>
2019-05-01 19:20:07 +0300

    Some minor additions for classification report and oob score

--- estimate.py
+++ estimate.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import utilities as ut
+import utilities2 as ut
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 datasetName = "mergedDataset.csv"
 
 features = ut.loadCSV(datasetName, lm=True)
@@ -22,8 +23,12 @@ def errorCheck():
                                 maks = col[j]
                 print i + " : " + str(maks)
 
-rf = RandomForestClassifier(n_estimators=100)
-rf.fit(features, labels)
+features_train , features_test , labels_train , labels_test = train_test_split(features,labels, test_size = 0.33,random_state = 42)
+
+rf = RandomForestClassifier(n_estimators=100,oob_score=True,max_features="log2",random_state=42)
+
+rf.fit(features_train, labels_train)
+rf_pred = rf.predict(features_test)
 
 importances = rf.feature_importances_
 featureNames = list(features)
@@ -36,3 +41,7 @@ for i in range(0, len(importances)):
 featureImportance.sort(key = lambda i : i[1], reverse = True)
 for i in featureImportance:
         print i[0] + ": " + str(100 * i[1])
+
+
+print classification_report(labels_test, rf_pred)
+print "OOB_Score :" , rf.oob_score_
